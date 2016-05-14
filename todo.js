123084fd022ecf8d573785ec248defdd0de0179f
Daniel Hsueh <dhsueh@Daniels-MacBook-Pro.local>
2016-05-13 20:06:18 -0700

    first commit

---
+++ todo.js
@@ -0,0 +1,20 @@
+var todoList = {
+
+  todos: ['item 1', 'item 2', 'item 3'],
+
+  displayTodos: function(){
+    console.log("My todos", this.todos);
+  },
+  addTodo: function (todo){
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  changeTodo: function(position, newValue){
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: function(position){
+    this.todos.splice(1, position);
+    this.displayTodos();
+  }
+};
